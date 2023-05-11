d236bd906c054deced4d05e983b5fded9aa70292
Yekan Ji <jiyekan0711@gmail.com>
2023-05-11 09:31:42 -0700

    fix bug, wraping things with clientOnly component

--- airbnb-clone/app/layout.tsx
+++ airbnb-clone/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
 
 const font = Nunito({ 
   subsets: ['latin'] 
@@ -19,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
